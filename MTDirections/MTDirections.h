f267e38e28245f9f454e092af5228988b7c02168
Matthias Tretter <matthias.tretter@gmail.com>
2011-12-27 22:17:33 +0100

    Fixed MTDirections import file

--- MTDirections/MTDirections.h
+++ MTDirections/MTDirections.h
@@ -14,7 +14,8 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-#import "MTDirectionRequest.h"
+#import "MTDirectionsRequest.h"
 #import "MTDirectionsRouteType.h"
 #import "MTWaypoint.h"
 #import "MKMapView+MTDirections.h"
+#import "MTDirectionsOverlay.h"
