8c12a0ed369301d72ed20cdd3bdd952f95b922b4
Matthias Tretter <matthias.tretter@gmail.com>
2012-03-19 14:06:37 +0100

    Improved Demo

--- MTDirectionsDemo/MTDirectionsDemo/MTDirectionsSampleViewController.m
+++ MTDirectionsDemo/MTDirectionsDemo/MTDirectionsSampleViewController.m
@@ -9,7 +9,7 @@
 #import "MTDirectionsSampleViewController.h"
 #import "MTDirections.h"
 
-@interface MTDirectionsSampleViewController ()
+@interface MTDirectionsSampleViewController () <MKMapViewDelegate>
 
 @property (nonatomic, strong) MTMapView *mapView;
 
@@ -19,6 +19,10 @@
 
 @synthesize mapView = _mapView;
 
+////////////////////////////////////////////////////////////////////////
+#pragma mark - Lifecycle
+////////////////////////////////////////////////////////////////////////
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
@@ -27,11 +31,18 @@
     return self;
 }
 
+////////////////////////////////////////////////////////////////////////
+#pragma mark - UIViewController
+////////////////////////////////////////////////////////////////////////
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
 	self.mapView = [[MTMapView alloc] initWithFrame:self.view.bounds];
     self.mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    self.mapView.delegate = self;
+    self.mapView.region = MKCoordinateRegionMake(CLLocationCoordinate2DMake(51.459596, -0.973277),
+                                                 MKCoordinateSpanMake(0.026846, 0.032959));
     [self.view addSubview:self.mapView];
 }
 
@@ -46,10 +57,15 @@
     CLLocationCoordinate2D from = CLLocationCoordinate2DMake(51.38713, -1.0316);
     CLLocationCoordinate2D to = CLLocationCoordinate2DMake(51.4554, -0.9742);
     
-    [self.mapView loadDirectionsFrom:from
-                                  to:to
-                           routeType:MTDirectionsRouteTypeFastestDriving
-                zoomToShowDirections:YES];
+    double delayInSeconds = 1.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self.mapView loadDirectionsFrom:from
+                                      to:to
+                               routeType:MTDirectionsRouteTypeFastestDriving
+                    zoomToShowDirections:YES];
+    });
 }
+
 
 @end
