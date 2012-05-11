67d329d98d78e875b22ad9250392b96b33f4b146
Matthias Tretter <matthias.tretter@gmail.com>
2012-05-11 16:25:40 +0200

    Added Unit test target

---
+++ MTDirectionsKitTest/MTDirectionsKitTest.h
@@ -0,0 +1,13 @@
+//
+//  MTDirectionsKitTest.h
+//  MTDirectionsKitTest
+//
+//  Created by Tretter Matthias on 11.05.12.
+//  Copyright (c) 2012 @myell0w. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MTDirectionsKitTest : SenTestCase
+
+@end
