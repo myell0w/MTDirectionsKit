40d2f1f532b85fca16e57445d57b08a7773e3d35
Matthias Tretter <matthias.tretter@gmail.com>
2012-07-29 03:06:41 -0700

    Add Unit-Test for Google Parser testing alternative routes. Not implemented yet, therefore the test fails

--- MTDirectionsKitTest/MTDDirectionsParserGoogleTest.m
+++ MTDirectionsKitTest/MTDDirectionsParserGoogleTest.m
@@ -3,6 +3,7 @@
 #import "MTDDistance.h"
 #import "MTDWaypoint.h"
 #import "MTDAddress.h"
+#import "MTDRoute.h"
 
 
 @implementation MTDDirectionsParserGoogleTest
@@ -101,5 +102,62 @@
         [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
     }
 }
+
+- (void)testAlternatives {
+    NSString *path = [[[NSBundle bundleForClass:[self class]] bundlePath] stringByAppendingPathComponent:@"google_alternatives.xml"];
+    NSData *data = [NSData dataWithContentsOfFile:path];
+
+    STAssertNotNil(data, @"Couldn't read google_alternatives.xml");
+
+    __block BOOL testFinished = NO;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        CLLocationCoordinate2D from = CLLocationCoordinate2DMake(40.339885, -75.926577);         // Reading USA
+        CLLocationCoordinate2D to = CLLocationCoordinate2DMake(38.895114, -77.036369);           // Washington D.C.
+
+        MTDDirectionsParserGoogle *parser = [[MTDDirectionsParserGoogle alloc] initWithFrom:[MTDWaypoint waypointWithCoordinate:from]
+                                                                                         to:[MTDWaypoint waypointWithCoordinate:to]
+                                                                          intermediateGoals:nil
+                                                                                  routeType:MTDDirectionsRouteTypeShortestDriving
+                                                                                       data:data];
+
+
+        [parser parseWithCompletion:^(MTDDirectionsOverlay *overlay, NSError *error) {
+            STAssertNil(error,@"There was an error parsing mapquest_guessing_vienna.xml");
+
+            // Check alternative routes
+            STAssertTrue(overlay.routes.count == 3, @"Error parsing alternative routes");
+
+            // Check fastest and shortest route
+            STAssertTrue(overlay.fastestRoute.timeInSeconds == 9466., @"Fastest route not working");
+            STAssertEqualsWithAccuracy([overlay.shortestRoute.distance distanceInMeasurementSystem:MTDMeasurementSystemMetric], 137.845, 0.001, @"Shortest route not working");
+
+            // Check shortest route
+            STAssertEquals(overlay.timeInSeconds, 9557., @"Error parsing time");
+            STAssertEqualObjects(overlay.formattedTime, @"2:39:17", @"Error formatting time");
+            STAssertTrue(overlay.waypoints.count == 99, @"Error parsing waypoints %d", overlay.waypoints.count);
+            STAssertEqualsWithAccuracy([overlay.distance distanceInMeasurementSystem:MTDMeasurementSystemMetric], 137.845, 0.001, @"Error parsing distance");
+
+            for (MTDRoute *route in overlay.routes) {
+                // Check Addresses
+                MTDAddress *fromAddress = route.from.address;
+                MTDAddress *toAddress = route.to.address;
+
+                STAssertEqualObjects(fromAddress.fullAddress, @"Reading", @"fromAddress: error parsing city");                STAssertEqualObjects(toAddress.city, @"Washington", @"toAddress: error parsing city");
+                STAssertEqualObjects(toAddress.fullAddress, @"District of Columbia", @"toAddress: error parsing state");
+
+                // Check copyright
+                NSString *copyright = [route.additionalInfo objectForKey:MTDAdditionalInfoCopyrightsKey];
+                STAssertEqualObjects(copyright, @"Kartendaten ©2012 Google", @"Error parsing copyright");
+            }
+            
+            testFinished = YES;
+        }];
+    });
+    
+    while (!testFinished) {
+        [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+    }
+}
+
 
 @end
