74ecaabb41212e39c832f8bc9e49b99d4be296ba
Matthias Tretter <matthias.tretter@gmail.com>
2012-08-10 11:18:59 -0700

    Add MTDWaypoint singleton for currentLocation

--- MTDirectionsKit/General/MTDWaypoint.m
+++ MTDirectionsKit/General/MTDWaypoint.m
@@ -18,6 +18,18 @@
     return [[[self class] alloc] initWithAddress:address];
 }
 
++ (MTDWaypoint *)waypointForCurrentLocation {
+    __strong static MTDWaypoint *waypointForCurrentLocation = nil;
+
+    static dispatch_once_t onceToken;
+
+    dispatch_once(&onceToken, ^{
+        waypointForCurrentLocation = [MTDWaypoint waypointWithCoordinate:kCLLocationCoordinate2DInvalid];
+    });
+
+    return waypointForCurrentLocation;
+}
+
 - (id)initWithCoordinate:(CLLocationCoordinate2D)coordinate {
     if ((self = [super init])) {
         _coordinate = coordinate;
@@ -68,6 +80,10 @@
 ////////////////////////////////////////////////////////////////////////
 
 - (NSString *)description {
+    if (self == [MTDWaypoint waypointForCurrentLocation]) {
+        return @"<MTDWaypoint currentLocation>";
+    }
+
     if (CLLocationCoordinate2DIsValid(self.coordinate)) {
         return [NSString stringWithFormat:@"<MTDWaypoint: %@>",
                 MTDStringFromCLLocationCoordinate2D(self.coordinate)];
@@ -80,6 +96,10 @@
 - (BOOL)isEqual:(id)object {
     if (![object isKindOfClass:[MTDWaypoint class]]) {
         return NO;
+    }
+
+    if (object == self) {
+        return YES;
     }
     
     MTDWaypoint *otherWaypoint = (MTDWaypoint *)object;
@@ -95,7 +115,7 @@
 }
 
 - (NSUInteger)hash {
-    if (CLLocationCoordinate2DIsValid(self.coordinate)) {
+    if (CLLocationCoordinate2DIsValid(self.coordinate) || self == [MTDWaypoint waypointForCurrentLocation]) {
         NSNumber *latitudeNumber = @(self.coordinate.latitude);
         NSNumber *longitudeNumber = @(self.coordinate.longitude);
         
